3be9cfd25bf0960473bf05567c28b31986331c9b
liuglaze <1823892985@qq.com>
2024-07-06 10:38:24 +0800

    单例

---
+++ Assets/Scripts/SystemModule/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineManager : MonoBehaviour
+{
+    private static CoroutineManager _instance;
+
+    public static CoroutineManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject("CoroutineManager");
+                _instance = obj.AddComponent<CoroutineManager>();
+                DontDestroyOnLoad(obj);
+            }
+            return _instance;
+        }
+    }
+}
